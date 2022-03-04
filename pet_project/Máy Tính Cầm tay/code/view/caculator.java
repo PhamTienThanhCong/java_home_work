d655debe823f24a3b48d650696fd534261fdf767
congphamtienthanh <congphamtienthanh@gmail.com>
2022-03-04 14:35:20 +0700

    create a bug

--- pet_project/Máy Tính Cầm tay/code/view/caculator.java
+++ pet_project/Máy Tính Cầm tay/code/view/caculator.java
@@ -17,10 +17,10 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
+// import com.jgoodies.forms.layout.FormLayout;
+// import com.jgoodies.forms.layout.ColumnSpec;
+// import com.jgoodies.forms.layout.FormSpecs;
+// import com.jgoodies.forms.layout.RowSpec;
 
 import controler.ControlerMath;
 import model.ValueModel;
