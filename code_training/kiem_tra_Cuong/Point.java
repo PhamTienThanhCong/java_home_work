5cfd4c82c549c2592214ad04c6b6defc51ee9a81
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-12 13:38:44 +0700

    BAi 2

---
+++ code_training/kiem_tra_Cuong/Point.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Constructor;
+
+public class Point {
+    public double x,y;
+
+    public Point() {
+    }
+
+    public Point(double x,double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+}
