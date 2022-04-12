3e3ce64e56e5ab5e90e5b50627bef39fa6a5c703
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-12 13:47:58 +0700

    bai 3

--- code_training/kiem_tra_Cuong/Point.java
+++ code_training/kiem_tra_Cuong/Point.java
@@ -32,4 +32,21 @@ public class Point {
         this.y = y;
     }
 
+    public double KhoangCach(Point p1, Point p2) {
+        double a = p2.x - p1.x;
+        double b = p2.y - p1.y;
+        return Math.sqrt(a*a + b*b);
+    }
+
+    public Boolean checkThangHang(Point a, Point b, Point c) {
+        double ab = KhoangCach(a,b);
+        double ac = KhoangCach(c,a);
+        double bc = KhoangCach(b,c);
+
+        if (ab > ac + bc || ac > ab + bc || bc > ac + ab){
+            return false;
+        }
+        return true;
+    }
+
 }
