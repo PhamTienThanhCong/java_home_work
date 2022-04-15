17935618ba086f75af37262eeec6eab5f9a1b663
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-15 09:38:58 +0700

    add file

---
+++ code_training/kiem_tra_Cuong/Point3d.java
@@ -0,0 +1,31 @@
+public class Point3d extends Point {
+    private double z;
+    public Point3d() {
+    }
+    public Point3d(double z) {
+        this.z = z;
+    }
+    public Point3d(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+    public Point3d(Point3d p) {
+        super(p.x, p.y);
+        this.z = z;
+    }
+    public Point3d(Point p, double z) {
+        super(p);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    
+
+}
